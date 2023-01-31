1170b25978fefadcb49103040c18369fcd2aecc0
Bernard Costa <biernard@gmail.com>
2023-01-31 23:47:47 +0000

    Just usual updates

---
+++ Javascript/meu-script.js
@@ -0,0 +1,8 @@
+//alert('Olá,seja bem vindo ao curso!')
+
+/*
+    selecionar um elemento do dom
+    atualizar o valor de uma linha*/
+    
+document.getElementById('nome').value = 'Oi'
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Desenvolvimento-Web
-Lessons and Practices
